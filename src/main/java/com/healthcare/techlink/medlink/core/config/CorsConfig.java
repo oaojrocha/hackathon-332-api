4b490a09d186d7747e0707c346cdeb0261cd1ea7
João Ricardo Rocha <joao.rochar@gmail.com>
2023-08-25 17:04:06 -0300

    Swagger

--- src/main/java/com/healthcare/techlink/medlink/core/config/CorsConfig.java
+++ src/main/java/com/healthcare/techlink/medlink/core/config/CorsConfig.java
@@ -1,8 +1,14 @@
 package com.healthcare.techlink.medlink.core.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -12,5 +18,20 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // Add your allowed origins here
                 .allowedMethods("*")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        converters.add(new StringHttpMessageConverter());
     }
 }
---
+++ src/main/java/com/healthcare/techlink/medlink/core/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.healthcare.techlink.medlink.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("MedLink")
+                        .description("HealthTech")
+                        .version("1.0"));
+    }
+}
