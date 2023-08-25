edd11360f2d485b559b2ff73ddfeba34596d1073
João Ricardo Rocha <joao.rochar@gmail.com>
2023-08-25 15:06:59 -0300

    Cors

--- src/main/java/com/healthcare/techlink/medlink/core/config/WebSecurityConfig.java
+++ src/main/java/com/healthcare/techlink/medlink/core/config/WebSecurityConfig.java
@@ -2,11 +2,11 @@ package com.healthcare.techlink.medlink.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebSecurity
+@EnableWebMvc
 @Configuration
 public class WebSecurityConfig {
     @Bean
@@ -17,8 +17,7 @@ public class WebSecurityConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
