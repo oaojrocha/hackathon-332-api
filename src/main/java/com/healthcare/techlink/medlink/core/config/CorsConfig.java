4f72121bd21c2e35bb7714b2ef73bbe0d6c0d4f5
João Ricardo Rocha <joao.rochar@gmail.com>
2023-08-25 16:17:12 -0300

    Cors

---
+++ src/main/java/com/healthcare/techlink/medlink/core/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.healthcare.techlink.medlink.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Add your allowed origins here
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
